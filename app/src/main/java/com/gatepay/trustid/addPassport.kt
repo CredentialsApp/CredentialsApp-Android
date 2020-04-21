a62bf66d283aefd345e01020298d591ce2b77f0b
Noel-96 <noelorion69@gmail.com>
2020-04-21 19:25:08 +0100

    prompt credential selection popover

---
+++ app/src/main/java/com/gatepay/trustid/addPassport.kt
@@ -0,0 +1,16 @@
+package com.gatepay.trustid
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class addPassport : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_passport)
+
+
+    }
+    }
