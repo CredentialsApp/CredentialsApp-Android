e88319753b981f811528c0fc2f8fced137d48c90
Noel-96 <noelorion69@gmail.com>
2020-04-23 20:31:43 +0100

    extracting deep link from dynamic link

--- app/src/main/java/com/gatepay/trustid/MainActivity.kt
+++ app/src/main/java/com/gatepay/trustid/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.gatepay.trustid
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 import android.widget.LinearLayout
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.dynamiclinks.ktx.dynamicLinks
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_my_crendentials.*
 
 
 class MainActivity : AppCompatActivity(), BottomSheetEx.BottomSheetListener {
 
-// TODO ADD ANDROID NATIVE BACK BUTTON
-// TODO IMPLEMENT BOTTOM SHEET
-// TODO fix bottom sheet design
 
 
     private lateinit var sheetBehavior: BottomSheetBehavior<LinearLayout>
@@ -25,6 +27,31 @@ class MainActivity : AppCompatActivity(), BottomSheetEx.BottomSheetListener {
                     val bottomSheet = BottomSheetEx()
                     bottomSheet.show(supportFragmentManager, "BottomSheetEx")
                 }
+
+
+
+        Firebase.dynamicLinks
+            .getDynamicLink(intent)
+            .addOnSuccessListener(this) { pendingDynamicLinkData ->
+                // Get deep link from result (may be null if no link is found)
+                var deepLink: Uri? = null
+                if (pendingDynamicLinkData != null) {
+                    deepLink = pendingDynamicLinkData.link
+                    Snackbar.make(
+                        findViewById(R.id.constraintlayout),
+                        deepLink.toString(),
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                }
+
+                // Handle the deep link. For example, open the linked
+                // content, or apply promotional credit to the user's
+                // account.
+                // ...
+
+                // ...
+            }
+            .addOnFailureListener(this) { e -> Log.w("TAG", "getDynamicLink:onFailure", e) }
     }
 
     override fun onOptionClick(text: String) {
