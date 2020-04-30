6564ed31ab2b9ca6d4fc7566e67b7606b29d08c0
Noel-96 <noelorion69@gmail.com>
2020-04-30 10:43:10 +0100

    ocr recognition  pending/ custom overlay design done

--- app/src/main/java/com/gatepay/trustid/CameraActivity.kt
+++ app/src/main/java/com/gatepay/trustid/CameraActivity.kt
@@ -1,13 +1,36 @@
 package com.gatepay.trustid
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import org.jmrtd.lds.icao.MRZInfo
 
-class CameraActivity  : AppCompatActivity() {
+class CameraActivity  : AppCompatActivity() , CameraMrzFragment.CameraMLKitCallback {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, CameraMrzFragment())
+            .commit()
     }
+
+    override fun onBackPressed() {
+        setResult(Activity.RESULT_CANCELED)
+        finish()
+    }
+
+    override fun onPassportRead(mrzInfo: MRZInfo) {
+//        val intent = Intent()
+//        intent.putExtra("KEY_MRZ_INFO", mrzInfo)
+//        setResult(Activity.RESULT_OK, intent)
+//        finish()
+    }
+
+    override fun onError() {
+        onBackPressed()
+    }
+
 }
